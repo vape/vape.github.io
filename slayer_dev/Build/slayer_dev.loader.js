4c6ae3f7aca7bfa5ed48cebfadcca30882d64f1b
Artem Sokolov <sokolov.artem@hotmail.com>
2025-03-30 12:32:03 +0200

    Update slayer_dev

--- slayer_dev/Build/slayer_dev.loader.js
+++ slayer_dev/Build/slayer_dev.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 52087226,
+    wasmFileSize: 52113071,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
