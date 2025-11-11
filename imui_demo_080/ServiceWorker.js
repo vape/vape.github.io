5dc1bc2b2684dfe7e44b5bf75ca6e031f732af66
Artem Sokolov <sokolov.artem@hotmail.com>
2025-11-11 13:07:30 +0100

    Add imui 0.8 demo

---
+++ imui_demo_080/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "deszz-Imui Demo-0.8";
+const contentToCache = [
+    "Build/imui_demo_080.loader.js",
+    "Build/imui_demo_080.framework.js",
+    "Build/imui_demo_080.data",
+    "Build/imui_demo_080.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
